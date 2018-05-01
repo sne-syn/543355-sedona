2f7b898466fa5ddfede7ad87adaddb4809f0d134
sne-syn <snezhana.syngur@gmail.com>
2018-05-01 09:53:23 +0200

    test

--- js/form.js
+++ js/form.js
@@ -1,20 +1,20 @@
  var link = document.querySelector(".hotel-button");
-    var form = document.querySelector(".booking-fields");
-    var checkIn = form.querySelector("[id=check-in]");
-    var checkOut = form.querySelector("[id=check-out]");
-    var roomAdults = form.querySelector("[id=adults-field]");
+ var form = document.querySelector(".booking-fields");
+ var checkIn = form.querySelector("[id=check-in]");
+ var checkOut = form.querySelector("[id=check-out]");
+ var roomAdults = form.querySelector("[id=adults-field]");
 
-    link.addEventListener("click", function(evt) {
-      evt.preventDefault();
-      form.classList.toggle("booking-fields-js");
-      checkIn.focus();
-    });
+ link.addEventListener("click", function (evt) {
+   evt.preventDefault();
+   form.classList.toggle("booking-fields-js");
+   checkIn.focus();
+ });
 
-    form.addEventListener("submit", function(evt) {
-      if (!checkIn.value || !checkOut.value || !roomAdults.value) {
-        evt.preventDefault();
-        form.classList.remove("booking-field-error");
-        form.offsetWidth = form.offsetWidth;
-        form.classList.add("booking-field-error");
-      }
-    });
+ form.addEventListener("submit", function (evt) {
+   if (!checkIn.value || !checkOut.value || !roomAdults.value) {
+     evt.preventDefault();
+     form.classList.remove("booking-field-error");
+     form.offsetWidth = form.offsetWidth;
+     form.classList.add("booking-field-error");
+   }
+ });
