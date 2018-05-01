d7e740f0e198ea8de5262ac407fb3a1be2459303
sne-syn <snezhana.syngur@gmail.com>
2018-05-01 22:10:59 +0200

    min

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,25 @@ form.addEventListener("submit", function (evt) {
     form.classList.add("booking-field-error");
   }
 });
+
+
+function initMap() {
+  var element = document.getElementsByClassName("background-map")[0];
+  var options = {
+    zoom: 10,
+    center: {
+      lat: 34.857323,
+      lng: -111.794933
+    }
+  };
+
+  var myMap = new google.maps.Map(element, options);
+
+  var marker = new google.maps.Marker({
+    position: {
+      lat: 34.857323,
+      lng: -111.794933
+    },
+    map: myMap
+  });
+}
